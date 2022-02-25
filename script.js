38baf824ce5a5be93d2af78424b3162db21b4ba4
lifeofsoumya <mondalsoumya2001@gmail.com>
2022-02-25 22:38:22 +0530

    basic functions mod, comments added

--- script.js
+++ script.js
@@ -1,18 +1,20 @@
 function convert() {
     // const signature = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', 0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
 
-    var input = document.getElementById("input-word").value;
-    var salt = document.getElementById("salt-input").value;
+    const input = document.getElementById("input-word").value;
+    const salt = document.getElementById("salt-input").value;
 
-    var inpArr = input.split('');
+    var inpArr = input.split(''); // converts input to an array, stores into inpArr
 
-    for (i = 0; i < inpArr.length; i++) {
-        var outSign = inpArr[i].charCodeAt(0) + salt;
-        var output = '' + outSign;
-        console.log(outSign)
+    var output = [] // output is a new blank array
+
+    for (i = 0; i < inpArr.length; i++) { // runs loop with input word array one by one character
+        var outSign = inpArr[i].charCodeAt(0) + salt; // converts input to ASCII and adds salt no to the ascii
+        output.push(outSign); // pushes each ascii as an array element
     }
-    
-    document.querySelector('#output-hash').innerHTML = output;
+    outputStr = output.join() // converts the output array into a string
+
+    document.querySelector('#output-hash').innerHTML = outputStr; // prints the output encrypted word
 }
 
 
